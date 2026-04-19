6bf4b82c33560c582730e462ceee67d4b265011c
kishanyadav5436 <kishankumar13580@gmail.com>
2026-04-19 17:21:37 +0530

    Fix: Configure project for Vercel deployment with Neon PostgreSQL

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Building the project..."
+python3.12 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.12 manage.py collectstatic --noinput --clear
